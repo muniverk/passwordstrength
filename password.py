8f77374a4cb3073df6a5099d63c2d036482c00af
muniverk <muniverkharod26@gmail.com>
2025-02-16 01:28:36 -0500

    Update password.py

--- password.py
+++ password.py
@@ -2,8 +2,6 @@
 ******************************
 CS 1026 - Assignment 2 – Password Strength
 Code by: Muniver Kaur Kharod
-Student ID: mkharod2
-File created: October 20, 2024
 ******************************
 This file is used to calculate the strength of a password based on the
 number of character groups it contains and its length. It includes a helper
